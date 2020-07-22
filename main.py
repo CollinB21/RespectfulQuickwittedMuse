2836eca3571913db1eac53ae235992bade423322
CollinB21 <collinbowser213@gmail.com>
2020-07-22 17:35:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+r = random.randint(-100,100)
+
+round = Falsewhile found = false
+while  found == false
+ n = int(input("guess the dang number"))
+
+  if n==r
+  found true
+  print("congradulations")
+ elif m>r:
+  print(str(n), "its greater")
+elif m<r:
+   print(str(m), "its less than it")
+   else:
+    print("Sorry not a number,please try again. ")
